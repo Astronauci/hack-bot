248063db5c2d426e38588c4a08d327f596975c24
Daniel Kosinski <de.kosinski@gmail.com>
2015-07-18 00:10:01 +0200

    Remove unused stuff

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import socket
 network = 'irc.freenode.net'
 channel = '#astronauci'
 nick = 'R2G2'
-chanMsg = "PRIVMSG %s :" % channel
 
 class HackBot(object):
 
@@ -40,5 +39,4 @@ class HackBot(object):
 
 if __name__ == "__main__":
     r2g2 = HackBot(network, channel, nick)
-    print r2g2.__dict__
     r2g2.main_loop()
