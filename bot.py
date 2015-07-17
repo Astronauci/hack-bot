e17fc8a58ab32f71cdd6d7ac438d2471becb97d8
Daniel Kosinski <de.kosinski@gmail.com>
2015-07-17 22:32:04 +0200

    Add basic skeleton for ircbot

---
+++ bot.py
@@ -0,0 +1,39 @@
+import socket
+#from bot_config import *
+
+network = 'irc.freenode.net'
+channel = '#astronauci'
+nick = 'R2G2'
+chanMsg = "PRIVMSG %s :" %channel
+
+alive = True
+
+def ping():
+    irc.send("PONG :Pong\n")
+
+def joinChan(chan):
+    irc.send("JOIN %s\n" %chan)
+
+def sendMsg(msg):
+    irc.send("PRIVMSG %s :%s\n" % channel, msg)
+
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+irc.connect((network,6667))
+irc.send("USER %s %s %s :salty bot\n" %(nick,nick,nick))
+irc.send("NICK %s\n" %nick)
+
+joinChan(channel)
+
+while alive:
+    ircmsg = irc.recv(2048)
+    ircmsg = ircmsg.strip('\n\r')
+    print ircmsg
+
+    if ":!status" in ircmsg:
+        irc.send(chanMsg+"ALL SYSTEMS ONLINE\n")
+
+    if ircmsg.find(":Hello "+nick) != -1:
+        irc.send(chanMsg+"Hello!\n")
+
+    if ircmsg.find("PING :") != -1:
+        ping()
