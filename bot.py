fee17bf0b093dfdcca10a44a238998775cf98d20
Daniel Kosinski <de.kosinski@gmail.com>
2015-07-18 00:11:27 +0200

    Add helper functions

--- bot.py
+++ bot.py
@@ -12,30 +12,35 @@ class HackBot(object):
         self.channel = channel
         self.nick = nick
         self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print "Estabilishing connection..."
         self.irc.connect((network, 6667))
         self.irc.send("")
         self.irc.send("USER %s %s %s :salty bot\n" % (self.nick, self.nick, self.nick))
         self.irc.send("NICK %s\n" % self.nick)
         self.irc.send("JOIN %s\n" % self.channel)
+        print "Connected!"
 
     def ping(self):
+        print "Got pinged, returning"
         self.irc.send("PONG :Pong\n")
 
     def sendMsg(self, msg):
-        self.irc.send("PRIVMSG %s :%s\n" % channel, msg)
+        self.irc.send("PRIVMSG {} :{}\n".format(self.channel, msg))
 
     def check_for_command(self, irc_msg, command, return_command):
         if irc_msg.find(command) != -1:
-            return return_command()
+            return_command()
 
     def main_loop(self):
         alive = True
+        print "Running main loop"
+        self.sendMsg("Established connection")
+        self.sendMsg("Running main loop...")
+        self.sendMsg("Hello world!")
         while alive:
             self.ircmsg = self.irc.recv(2048)
             self.ircmsg = self.ircmsg.strip('\n\r')
-
-            if self.ircmsg.find("PING :") != -1:
-                self.ping()
+            self.check_for_command(self.ircmsg, "PING :", self.ping)
 
 if __name__ == "__main__":
     r2g2 = HackBot(network, channel, nick)
